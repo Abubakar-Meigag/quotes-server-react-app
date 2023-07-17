95a76861f0e0b5d40db8e95fa59852cb89769d66
Abubakar-Meigag <dr.sco0ot.77@gmail.com>
2023-07-17 19:29:34 +0100

    update component
    try to divided component based on the section

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import './App.css';
-import FetchData from './FetchData';
+import React from 'react';
+import FetchData from "./App/Fetch/FetchData";
+import "./App.css";
 
-function App() {
-  return (
-    
-    <FetchData />
-  )
+
+// function App() {
+//   return (
+//     <FetchData />
+//   )
+// }
+
+// export default App
+
+const App = () => {
+  return <FetchData />;
 }
 
 export default App;
