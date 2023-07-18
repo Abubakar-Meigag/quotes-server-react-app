dd0715250ca5cc9834f9f0a5bdaedede1b43dc25
Abubakar-Meigag <dr.sco0ot.77@gmail.com>
2023-07-18 11:35:21 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,15 +2,6 @@ import React from 'react';
 import FetchData from "./App/Fetch/FetchData";
 import "./App.css";
 
-
-// function App() {
-//   return (
-//     <FetchData />
-//   )
-// }
-
-// export default App
-
 const App = () => {
   return <FetchData />;
 }
